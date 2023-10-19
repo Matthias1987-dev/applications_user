c165f66b0438abb87c67436ab845716ea8460ab6
matthias1987-dev <dachs.matthias@googlemail.com>
2023-10-19 12:47:51 +0200

    Ausgabe über x und y Tabelle

--- mad_test_button_panel_on_off/mad_button_panel_on_off.c
+++ mad_test_button_panel_on_off/mad_button_panel_on_off.c
@@ -2,9 +2,15 @@
 #include <furi_hal.h>
 
 #include <gui/gui.h>
+#include <gui/view.h>
 #include <input/input.h>
 #include "mad_test_button_panel_on_off_icons.h"
 
+typedef struct {
+    uint8_t x, y;
+} ImagePosition;
+
+static ImagePosition image_position = {.x = 1, .y = 1};
 
 
 void my_draw_background(Canvas* canvas, void* context) {
@@ -67,7 +73,10 @@ void my_draw_enter_8(Canvas* canvas, void* context) {
     canvas_draw_icon(canvas, 99, 35, &I_On_hvr_25x27);
 }
 
-int mad_test_button_panel_on_off_app(void* p) {
+
+
+
+int32_t mad_test_button_panel_on_off_app(void* p) {
     UNUSED(p);
 
     //Show directions to user.
@@ -78,21 +87,28 @@ int mad_test_button_panel_on_off_app(void* p) {
 
 
 
-int pos = 1;
-do {
-if (pos == 1){view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);}
-if (pos == 2){view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);}
-if (pos == 3){view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);}
-if (pos == 4){view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);}
-if (pos == 5){view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);}
-if (pos == 6){view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);}
-if (pos == 7){view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);}
-if (pos == 8){view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);}
+    bool running = true;
+    while(running) {
 
-        furi_delay_ms(1000);
-        pos++;
-while (pos == 8);
-}
+    if (image_position.y == 1 && image_position.y == 1) {
+        view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);
+    } else if (image_position.y == 2 && image_position.y == 1) {
+        view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);
+    } else if (image_position.y == 1 && image_position.y == 2) {
+        view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);
+    } else if (image_position.y == 2 && image_position.y == 2) {
+        view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);
+    } else if (image_position.y == 1 && image_position.y == 3) {
+        view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);
+    } else if (image_position.y == 2 && image_position.y == 3) {
+        view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);
+    } else if (image_position.y == 1 && image_position.y == 4) {
+        view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);
+    } else if (image_position.y == 2 && image_position.y == 4) {
+        view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);
+    }
+
+} //end while(running);
 
     gui_remove_view_port(gui, view_port);
     return 0;
