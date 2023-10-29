b5c36ff8b51b0262366dc0af1883737794ae7b63
matthias1987-dev <dachs.matthias@googlemail.com>
2023-10-29 09:16:01 +0100

    Change Menu order and include in app.c/mad_button

--- mad_test_button_panel_on_off/mad_button_panel_on_off.c
+++ mad_test_button_panel_on_off/mad_button_panel_on_off.c
@@ -1,8 +1,16 @@
 #include <furi.h>
 #include <furi_hal.h>
-#include <stdlib.h>
 #include <gui/gui.h>
 #include <gui/view.h>
+#include <gui/view_dispatcher.h>
+#include <gui/modules/submenu.h>
+#include <gui/modules/text_input.h>
+#include <gui/modules/widget.h>
+#include <gui/modules/variable_item_list.h>
+#include <notification/notification.h>
+#include <notification/notification_messages.h>
+
+#include <stdlib.h>
 #include <input/input.h>
 #include "mad_test_button_panel_on_off_icons.h"
 
