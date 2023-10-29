139f1710e2b7659132e333c7926d8bb3f8ec5777
matthias1987-dev <dachs.matthias@googlemail.com>
2023-10-29 10:21:10 +0100

    Copy typedef from Skeleton_App

--- mad_test_button_panel_on_off/mad_button_panel_on_off.c
+++ mad_test_button_panel_on_off/mad_button_panel_on_off.c
@@ -14,6 +14,57 @@
 #include <input/input.h>
 #include "mad_test_button_panel_on_off_icons.h"
 
+#define TAG "SubGhz Remote"
+
+// Change this to BACKLIGHT_AUTO if you don't want the backlight to be continuously on.
+#define BACKLIGHT_Auto
+
+// Our application menu has 3 items.  You can add more items if you want.
+typedef enum {
+    SubGhzRemoteSubmenuIndexPlay,
+    SubGhzRemoteSubmenuIndexConfigure,
+    SubGhzRemoteSubmenuIndexAbout,
+} SubGhzRemoteSubmenuIndex;
+
+// Each view is a screen we show the user.
+typedef enum {
+    SubGhzRemoteViewTextInput, // Input for configuring text settings
+    SubGhzRemoteViewSubmenu, // The menu when the app starts
+    SubGhzRemoteViewConfigure, // The configuration screen
+    SubGhzRemoteViewPlay, // The main screen
+    SubGhzRemoteViewAbout, // The about screen with directions, link to social channel, etc.
+} SubGhzRemoteView;
+
+// Not use for the moment.
+/**
+ * typedef enum {
+    SubGhzRemoteEventIdRedrawScreen = 0, // Custom event to redraw the screen
+    SubGhzRemoteEventIdOkPressed = 42, // Custom event to process OK button getting pressed down
+} SubGhzRemoteEventId;
+*/
+
+typedef struct {
+    ViewDispatcher* view_dispatcher; // Switches between our views
+    NotificationApp* notifications; // Used for controlling the backlight
+    Submenu* submenu; // The application menu
+    TextInput* text_input; // The text input screen
+    VariableItemList* variable_item_list_config; // The configuration screen
+    View* view_play; // The main screen
+    Widget* widget_about; // The about screen
+
+    VariableItem* setting_2_item; // The name setting item (so we can update the text)
+    char* temp_buffer; // Temporary buffer for text input
+    uint32_t temp_buffer_size; // Size of temporary buffer
+
+    FuriTimer* timer; // Timer for redrawing the screen
+} SubGhzRemoteApp;
+
+typedef struct {
+    uint32_t setting_1_index; // The team color setting index
+    FuriString* setting_2_name; // The name setting
+    uint8_t x; // The x coordinate
+} SubGhzRemotePlayModel;
+
 /**
 // This is the position of the image on the screen
 // 1,1 is the first line left
