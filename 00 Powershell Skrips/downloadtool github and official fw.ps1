b49ba22ced713b49cc37514298a68a58c0a12133
matthias1987-dev <dachs.matthias@googlemail.com>
2024-02-04 14:25:21 +0100

    finish downloadtool github and official fw.ps1

--- 00 Powershell Skrips/downloadtool github and official fw.ps1
+++ 00 Powershell Skrips/downloadtool github and official fw.ps1
@@ -1,6 +1,6 @@
 write-host "Installation aktuelle Firmware:/ Installation current official Firmware" -ForegroundColor Green
 $folder = Read-Host "Gib den Ordnernamen ein./ Name of the main Folder."
-Write-Host "Der Ordner $folder wird erstellt./ Greate new folder $folder." -ForegroundColor Green
+Write-Host "Der Ordner $folder wird erstellt./ Create new folder $folder." -ForegroundColor Green
 git clone https://github.com/flipperdevices/flipperzero-firmware.git $folder
 Write-Host "Download der Firmware fertig. Ordner $folder wurde erstellt./ Finish Download of the Firmware. New Folder $folder greatet" -ForegroundColor Green
 Set-Variable -Name folder1 -Value (Get-Location)
@@ -15,7 +15,7 @@ Write-Host ".\fbt vscode_dist wird gestartet./ .\fbt vscode_dist starting." -For
 .\fbt vscode_dist
 Write-Host ".\fbt vscode_dist Fertig./ .\fbt vscode_dist finish" -ForegroundColor Green
 $read = Read-Host "In VSC öffnen ? Mit j bestätigen!/ Open in VSC? press j."
-if ($read -eq "j"){
-    $fullPath = Resolve-Path .\$folder
+if ($read -eq "j") {
+    $fullPath = Resolve-Path (Get-Location)
     code $fullPath
 }
